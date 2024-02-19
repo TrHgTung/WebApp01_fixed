1375e12dfdfddf8fe48ec32ba7e7992857505442
Agile101hcmue <89836403+TrHgTung@users.noreply.github.com>
2024-02-19 10:36:15 +0700

    fixed role auth

---
+++ Areas/Admin/Controllers/StatController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using WebApp01.Models;
+using WebApp01.Repository;
+
+namespace WebApp01.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = "Admin")]
+    public class StatController : Controller
+    {
+      
+
+        private readonly DataContext _dataContext;
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        public StatController(DataContext context, IWebHostEnvironment webHostEnvironment)
+        {
+            _dataContext = context;
+            _webHostEnvironment = webHostEnvironment;
+        }
+        public IActionResult Index()
+        {
+            Models.ProductModel model;
+           //int count = model.Count;
+            return View();
+        }
+        
+    }
+}
---
+++ Models/UserModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApp01.Models
+{
+	public class UserModel
+	{
+		[Key]
+		public int Id { get; set; }
+		[Required(ErrorMessage ="Vui lòng nhập tên người dùng")]
+		public string Username { get; set; }
+		[Required(ErrorMessage = "Vui lòng nhập email"),EmailAddress]
+		public string Email { get; set; }
+		[DataType(DataType.Password),Required(ErrorMessage = "Vui lòng nhập mật khẩu")]
+		public string Password { get; set; }
+	}
+}
